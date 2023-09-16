24dffdad77e43dcf92fd1deb32f40902bad92326
nerufuyo <listyoadipamungkas1401@gmail.com>
2023-09-16 08:56:41 +0700

    ✨feature: initial screen done

---
+++ lib/styles/pallet.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// Primary
+const Color primary10 = Color(0xFFFDECF0);
+const Color primary20 = Color(0xFFFCDFE4);
+const Color primary30 = Color(0xFFF7B0BF);
+const Color primary40 = Color(0xFFF289A0);
+const Color primary50 = Color(0xFFEE6180);
+const Color primary60 = Color(0xFFEA3A60);
+const Color primary70 = Color(0xFFBB2E4D);
+const Color primary80 = Color(0xFF8C233A);
+const Color primary90 = Color(0xFF5E1726);
+const Color primary100 = Color(0xFF2F0C13);
+
+// Secondary
+const Color secondary10 = Color(0xFFE6F2F7);
+const Color secondary20 = Color(0xFFCCE4F0);
+const Color secondary30 = Color(0xFF99CAE1);
+const Color secondary40 = Color(0xFF66AFD1);
+const Color secondary50 = Color(0xFF3395C2);
+const Color secondary60 = Color(0xFF007AB3);
+const Color secondary70 = Color(0xFF00628F);
+const Color secondary80 = Color(0xFF00496B);
+const Color secondary90 = Color(0xFF003148);
+const Color secondary100 = Color(0xFF001824);
+
+// Text
+const Color text0 = Color(0xFFFFFFFF);
+const Color text10 = Color(0xFFEAEAEA);
+const Color text20 = Color(0xFFD4D4D4);
+const Color text30 = Color(0xFFA9A9A9);
+const Color text40 = Color(0xFF7E7E7E);
+const Color text50 = Color(0xFF535353);
+const Color text60 = Color(0xFF282828);

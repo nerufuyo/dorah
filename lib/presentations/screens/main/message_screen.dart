c56e24a36bd4462854d075689815baaa886ba80d
nerufuyo <listyoadipamungkas1401@gmail.com>
2023-09-30 04:52:21 +0700

    📄 docs: message screen

--- lib/presentations/screens/main/message_screen.dart
+++ lib/presentations/screens/main/message_screen.dart
@@ -1,3 +1,6 @@
+import 'package:dorah/presentations/widgets/components.dart';
+import 'package:dorah/styles/pallet.dart';
+import 'package:dorah/styles/typography.dart';
 import 'package:flutter/material.dart';
 
 class MessageScreen extends StatefulWidget {
@@ -9,8 +12,31 @@ class MessageScreen extends StatefulWidget {
 }
 
 class _MessageScreenState extends State<MessageScreen> {
+  final List<Map<String, String>> getUsers = [];
+  final List<Map<String, String>> getMessages = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _buildHeader(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildHeader() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: customText(
+        customTextValue: 'Message',
+        customTextStyle: heading3.copyWith(color: text60),
+      ),
+    );
   }
 }
